5155cb22314f63e3d093908939a39eafd259fbe7
Dan Cooke <dcooke@stevens.edu>
2021-05-16 18:39:38 -0400

    frame extraction and inputs

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
 from PIL import Image
+import cv2
 import numpy
 
-image = Image.open('test_img.jpg')
-new_image = image.resize((192, 108))
-new_image.save('small.jpg')
+vid_in = input('Enter the video file name: ')
+img_in = input('ENter the base image file name: ')
 
-I = numpy.asarray(Image.open('small.jpg'))
+image = Image.open(img_in)
+img_out = image.resize((192, 108))
+img_out.save('output.jpg')
+
+cap = cv2.VideoCapture(vid_in)
+i = 0
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite('frames/' + str(i) + '.jpg',frame)
+    i += 1
+cap.release()
+cv2.destroyAllWindows()
